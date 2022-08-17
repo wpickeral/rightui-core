dd0cc49a0268401152673867b989267715023822
williampickeral <willpickeral@gmail.com>
2022-08-17 16:31:02 -0400

    updated Switch story to have inline docs. Updated the readme to reduce redundancy

--- packages/Switch/.storybook/main.js
+++ packages/Switch/.storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
   'stories': [
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   'addons': [
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-jest'
   ],
   'framework': '@storybook/react',
   'core': {
--- packages/TreeView/.storybook/main.js
+++ packages/TreeView/.storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   'stories': [
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   'addons': [
     '@storybook/addon-links',
