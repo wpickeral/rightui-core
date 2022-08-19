c6861c5a940e03ab7b28407b9e15b8f4ac2bbd90
Will Pickeral <willpickeral@gmail.com>
2022-08-18 21:48:04 -0400

    update fill-rule attribute to fillRule

--- packages/TreeView/src/TreeView.stories.tsx
+++ packages/TreeView/src/TreeView.stories.tsx
@@ -82,7 +82,7 @@ const ChevronExpand = () => {
       <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16'
            fill='currentColor' className='bi bi-chevron-expand'
            viewBox='0 0 16 16'>
-        <path fill-rule='evenodd'
+        <path fillRule='evenodd'
               d='M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z'/>
       </svg>);
 };
@@ -92,7 +92,7 @@ const ChevronCollapse = () => {
       <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16'
            fill='currentColor' className='bi bi-arrows-collapse'
            viewBox='0 0 16 16'>
-        <path fill-rule='evenodd'
+        <path fillRule='evenodd'
               d='M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8zm7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0zm-.5 11.707-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0v-3.793z'/>
       </svg>
   );
